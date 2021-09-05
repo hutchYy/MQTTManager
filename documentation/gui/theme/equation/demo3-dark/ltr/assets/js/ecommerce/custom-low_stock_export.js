ff5644bc09ea5734f998a8a9f57cdd272e24e2eb
hutchYy <julian.dotreppe@gmail.com>
2021-09-05 22:10:08 +0200

    removing theme files

--- documentation/gui/theme/equation/demo3-dark/ltr/assets/js/ecommerce/custom-low_stock_export.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'csv', 'excel'
-        ]
-    } );
-} );
--- documentation/gui/theme/equation/demo3-dark/ltr/plugins/bootstrap-colorpicker/bootstrap-colorpicker-custom.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function() {
-
-//     var a = $("body")[0].style;
-//     $("#colorpicker-event").colorpicker().on("changeColor", function(b) {
-//         a.backgroundColor = b.color.toHex()
-//     });
-
-// })
